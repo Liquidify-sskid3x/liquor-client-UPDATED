c4b27f87246e6c49895593e10cffe3ede3e1a0e6
Liquidify <ps5mv59frx@privaterelay.appleid.com>
2026-05-30 23:00:09 +0300

    Delete src/main/java/com/example directory

--- src/main/java/com/example/liquorclient/module/BooleanSetting.java
+++
@@ -1,7 +0,0 @@
-package com.example.liquorclient.module;
-
-public class BooleanSetting extends Setting<Boolean> {
-    public BooleanSetting(String name, Boolean value) {
-        super(name, value);
-    }
-}
--- src/main/java/com/example/liquorclient/module/KeybindSetting.java
+++
@@ -1,9 +0,0 @@
-package com.example.liquorclient.module;
-
-import org.lwjgl.glfw.GLFW;
-
-public class KeybindSetting extends Setting<Integer> {
-    public KeybindSetting() {
-        super("Keybind", GLFW.GLFW_KEY_UNKNOWN);
-    }
-}
